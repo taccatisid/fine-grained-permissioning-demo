51b33eea1921288d5ae2df4c7513e0b29ae5de0a
Taccat Isid <taccatisid@protonmail.com>
2021-11-12 11:57:18 +1300

    add demo script
    Signed-off-by: Taccat Isid <taccatisid@protonmail.com>

---
+++ demo.js
@@ -0,0 +1,29 @@
+// A part of the AccountRules API
+accountRulesAbi = [{"name":"addAccount","type":"function","inputs":[{"internalType":"address","name":"account","type":"address"}],"outputs":[],"stateMutability":"nonpayable"},{"name":"setCreateContractPermission","type":"function","inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"bool","name":"allowed","type":"bool"}],"outputs":[],"stateMutability":"nonpayable"}]
+
+// Prefunded accounts to play with
+adminAccount = accounts[0];
+internalAccount = accounts[1];
+externalAccount = accounts[2];
+
+// Contract instances
+DirectDemo.at(process.env.CONTRACT_DIRECT).then(result => directDemo = result);
+DelegateDemo.at(process.env.CONTRACT_DELEGATED).then(result => delegateDemo = result);
+CentralAccessControl.at(process.env.CONTRACT_CENTRAL_AUTHORITY).then(result => centralAuthority = result);
+accountRules = new web3.eth.Contract(accountRulesAbi, process.env.CONTRACT_ACCOUNT_RULES);
+
+// The role our contracts use to discern writing permission
+writerRole = web3.utils.keccak256("WRITER_ROLE");
+
+// Things to try:
+// - await accountRules.methods.addAccount(internalAccount).send({from: adminAccount});
+// - await accountRules.methods.setCreateContractPermission(internalAccount, true).send({from: adminAccount});
+// - await DirectDemo.new({from: internalAccount})
+// - directDemo.grantRole(writerRole, internalAccount, {from: adminAccount})
+// - directDemo.write(99, {from: internalAccount})
+// - centralAuthority.grantRole(writerRole, internalAccount, {from: adminAccount})
+// - delegateDemo.write(10, {from: internalAccount})
+// - delegateDemo.read({from: externalAccount})
+// - centralAuthority.revokeRole(writerRole, internalAccount, {from: adminAccount})
+
+'lets start'
---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+source accounts.env
+
+set -x
+
+yarn install
+(cd permissioning-smart-contract && yarn install)
+(cd permissioning-smart-contract && yarn truffle migrate --network demo)
+yarn truffle migrate --network demo
+
+set +x
+echo "
+All contracts have been deployed.
+
+Now run
+  source accounts.env
+  source contracts.env
+  yarn truffle console --network demo
+and once it had started load the rest of the demo using
+  .load demo.js
+and follow the instructions from there."
