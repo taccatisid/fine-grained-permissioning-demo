25b5c5510bedd57ec461ab616b1ce69296e3baa7
Frank Li <b439988l@gmail.com>
2022-03-21 13:35:06 +1000

    remove dapp

--- permissioning-smart-contract/src/constants/tabs.ts
+++
@@ -1,8 +0,0 @@
-export const ADMIN_TAB = 'adminTab';
-export const ENODE_TAB = 'enodeTab';
-export const ACCOUNT_TAB = 'accountTab';
-export default [
-  { id: ACCOUNT_TAB, text: 'Accounts' },
-  { id: ENODE_TAB, text: 'Nodes' },
-  { id: ADMIN_TAB, text: 'Admins' }
-];
--- permissioning-smart-contract/src/setupTests.ts
+++
@@ -1,6 +0,0 @@
-// Enzyme setup
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'jest';
-
-configure({ adapter: new Adapter() });
